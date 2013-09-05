2f6f89792194c355354c7d1c38ad9d6285f34086
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-09-05 17:36:48 +0200

    Changed unit tests layout

--- tea_party/tests/__init__.py
+++ tea_party/tests/__init__.py
@@ -1,27 +1,3 @@
 """
-tea-party unit tests.
+Unit tests module.
 """
-
-import os
-import unittest
-
-from tea_party.party import load_party_file
-
-
-class TestTeaParty(unittest.TestCase):
-
-    def setUp(self):
-        self.fixtures_path = os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__),
-                'fixtures'
-            )
-        )
-        self.party_file = os.path.join(self.fixtures_path, 'party.yaml')
-
-    def test_parsing(self):
-        party = load_party_file(self.party_file)
-
-        attendees_names = set([attendee.name for attendee in party.attendees])
-
-        self.assertEqual(set(['boost', 'libiconv', 'libfoo']), attendees_names)
