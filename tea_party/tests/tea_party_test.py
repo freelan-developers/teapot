a4146b21479f7cc7077e48c2eb407cea635a1022
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-09-05 18:45:47 +0200

    Revert "Debug unit tests..."
    This reverts commit da13888ef18ee5424a5503244eee777cbcf01980.

--- tea_party/tests/tea_party_test.py
+++ tea_party/tests/tea_party_test.py
@@ -9,26 +9,23 @@ try:
 except ImportError:
     import unittest
 
-try:
-    from tea_party.party import load_party_file
+from tea_party.party import load_party_file
 
 
-    class TestTeaParty(unittest.TestCase):
+class TestTeaParty(unittest.TestCase):
 
-        def setUp(self):
-            self.fixtures_path = os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__),
-                    'fixtures'
-                )
+    def setUp(self):
+        self.fixtures_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                'fixtures'
             )
-            self.party_file = os.path.join(self.fixtures_path, 'party.yaml')
+        )
+        self.party_file = os.path.join(self.fixtures_path, 'party.yaml')
 
-        def test_parsing(self):
-            party = load_party_file(self.party_file)
+    def test_parsing(self):
+        party = load_party_file(self.party_file)
 
-            attendees_names = set([attendee.name for attendee in party.attendees])
+        attendees_names = set([attendee.name for attendee in party.attendees])
 
-            self.assertEqual(set(['boost', 'libiconv', 'libfoo']), attendees_names)
-except Exception as ex:
-    print ex
+        self.assertEqual(set(['boost', 'libiconv', 'libfoo']), attendees_names)
