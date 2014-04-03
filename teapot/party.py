8812d981d44f006227890aa5477419d60989f969
Julien Kauffmann <julien.kauffmann@freelan.org>
2014-04-03 16:08:06 +0200

    Disabled bytecode generation

--- teapot/party.py
+++ teapot/party.py
@@ -3,7 +3,10 @@ The party classes.
 """
 
 import os
+import sys
 import imp
+
+from contextlib import contextmanager
 
 from .log import LOGGER
 
@@ -18,7 +21,18 @@ def load_party_file(path):
     path = os.path.abspath(path)
 
     LOGGER.debug('Importing party file from %r', path)
-    imp.load_source('party', path)
+
+    @contextmanager
+    def disable_bytecode_generation():
+        sentinel, sys.dont_write_bytecode = sys.dont_write_bytecode, True
+
+        try:
+            yield
+        finally:
+            sys.dont_write_bytecode = sentinel
+
+    with disable_bytecode_generation():
+        imp.load_source('party', path)
 
 
 def fetch(attendees=None, force=False):
